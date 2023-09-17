2685ccac78c485579ba67624c195987331a15ff9
Nexadis <37233902+Nexadis@users.noreply.github.com>
2023-09-17 18:07:12 +0300

    Observer

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"strings"
+	"sync"
+	"time"
 )
 
 func NewIter(max int) func() (int, bool) {
@@ -28,9 +30,42 @@ func DemoIterator() {
 	}
 }
 
+type Subscriber struct {
+	ID int
+}
+
+// Subscribe ожидает уведомления.
+func (s Subscriber) Subscribe(c *sync.Cond) {
+	for {
+		c.L.Lock()
+		c.Wait()
+		fmt.Printf("Subscriber %v is notified\n", s.ID)
+		c.L.Unlock()
+	}
+}
+
+func DemoObserver() {
+	cond := sync.NewCond(new(sync.Mutex))
+	s1 := Subscriber{1}
+	go s1.Subscribe(cond)
+	s2 := Subscriber{2}
+	go s2.Subscribe(cond)
+	s3 := Subscriber{3}
+	go s3.Subscribe(cond)
+	time.Sleep(200 * time.Millisecond)
+	// отправка уведомлений всем подписчикам
+	cond.Broadcast()
+	time.Sleep(200 * time.Millisecond)
+	fmt.Println("Once more")
+	cond.Broadcast()
+	time.Sleep(200 * time.Millisecond)
+}
+
 func main() {
 	border("Iterator")
 	DemoIterator()
+	border("Observer")
+	DemoObserver()
 }
 
 func border(name string) {
